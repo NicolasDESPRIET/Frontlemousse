cd8f45dde4f2beea5d940908748478c0c8e85552
FINZY <william.finzy@capgemini.com>
2021-12-22 15:09:12 +0100

    Adjust content padding and navbar position

--- pamplefront/src/layout/navbar/navbar.component.ts
+++ pamplefront/src/layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { constantsSharedTexts, constantsSharedButtons } from 'src/shared/shared-text';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  titleNav = constantsSharedTexts.BRAND_NAME;
+  navBackHomeText = constantsSharedButtons.BACK_HOME_BUTTON;
+
+  constructor() {
+    // Empty
+  }
 
   ngOnInit(): void {
   }
