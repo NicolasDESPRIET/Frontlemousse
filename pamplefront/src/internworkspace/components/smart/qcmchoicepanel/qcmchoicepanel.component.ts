b25bd3ed944972c0cfccfd3fa99abd652059fb90
FINZY <william.finzy@capgemini.com>
2022-01-17 15:45:03 +0100

    Create qcm choice panel and qcm card

---
+++ pamplefront/src/internworkspace/components/smart/qcmchoicepanel/qcmchoicepanel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qcmchoicepanel',
+  templateUrl: './qcmchoicepanel.component.html',
+  styleUrls: ['./qcmchoicepanel.component.scss']
+})
+export class QcmchoicepanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
