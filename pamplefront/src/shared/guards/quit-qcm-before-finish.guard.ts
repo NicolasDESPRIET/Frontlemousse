73997d0707bbd0455c48c0ec1387579cafb77df0
FINZY <william.finzy@capgemini.com>
2022-01-28 10:58:10 +0100

    Finish guard for quit qcm

--- pamplefront/src/shared/guards/quit-qcm-before-finish.guard.ts
+++ pamplefront/src/shared/guards/quit-qcm-before-finish.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import {CanDeactivate } from '@angular/router';
+import { fakeAsync } from '@angular/core/testing';
+import {CanDeactivate, Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { DoqcmpageComponent } from 'src/internworkspace/components/pages/doqcmpage/doqcmpage.component';
 import { DoqcmComponent } from 'src/internworkspace/components/smart/doqcm/doqcm.component';
 
 export interface ICanComponentDeactivate {
-  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+  canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean | UrlTree;
 }
 
 @Injectable({
@@ -13,6 +14,8 @@ export interface ICanComponentDeactivate {
 })
 export class QuitQcmBeforeFinishGuard implements CanDeactivate<DoqcmComponent> {
   
+  constructor(private router: Router){}
+
   canDeactivate(component: DoqcmComponent) {
     return component.canDeactivate();
   }
