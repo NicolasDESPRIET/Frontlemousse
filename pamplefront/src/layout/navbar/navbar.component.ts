311d5a1c239d1a3aa1139082e2de036663de3319
FINZY <william.finzy@capgemini.com>
2022-02-10 19:45:57 +0100

    Add dropdown menu in navbar

--- pamplefront/src/layout/navbar/navbar.component.ts
+++ pamplefront/src/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { constantsSharedTexts, constantsSharedButtons } from 'src/shared/shared-text';
 
 @Component({
@@ -8,14 +10,59 @@ import { constantsSharedTexts, constantsSharedButtons } from 'src/shared/shared-
 })
 export class NavbarComponent implements OnInit {
 
+  // Text
   titleNav = constantsSharedTexts.BRAND_NAME;
   navBackHomeText = constantsSharedButtons.BACK_HOME_BUTTON;
+  regexName: RegExp = /[A-Za-z]+/i;
+  regexFamilyname: RegExp = /\s[A-Za-z]+/i;
 
-  constructor() {
+  /**
+   * Session variable
+   * @Value can be 'intern' or 'admin'
+   */
+  session = "intern";
+  sessionName = "";
+
+  userName: string = "Maurice HELLOWORLD";
+  isUserLoggedIn = true;
+  
+  nameInitial = this.userName.match(this.regexName)![0][0];
+  familyNameInitial = this.userName.match(this.regexFamilyname)![0][1];
+  initials: string =  this.nameInitial + this.familyNameInitial;
+
+  constructor(private router: Router, private dialog: MatDialog) {
     // Empty
   }
 
   ngOnInit(): void {
+    if(this.session === "intern") 
+    {this.sessionName = "stagiaire"}
+    else
+    {this.sessionName = "administrateur"}
+  }
+
+  navigateToWorkspace(){
+    if(this.session === "intern") 
+    {
+      this.router.navigate(["/intern"]);
+    }
+    else
+    {
+      this.router.navigate(["/admin"]);
+    }
+  }
+
+  openDecoPopup(templateRef: TemplateRef<any>){
+    this.dialog.open(templateRef, {
+      minWidth: '50vw'
+    });
+  }
+
+  logout(){
+    this.router.navigate(["/auth"])
+    // When auth is done it will be needed to uncomment the code below
+    //this.isUserLoggedIn = false;
+
   }
 
 }
