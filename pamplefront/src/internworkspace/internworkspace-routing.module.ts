3486e7aad08b0fbb60448f95611669b8324f0aa6
FINZY <william.finzy@capgemini.com>
2022-02-19 16:14:59 +0100

    Add guards to components to manage authentication

--- pamplefront/src/internworkspace/internworkspace-routing.module.ts
+++ pamplefront/src/internworkspace/internworkspace-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuitQcmBeforeFinishGuard } from 'src/shared/guards/quit-qcm-before-finish.guard';
+import { RedirectToLoginPageGuard } from 'src/shared/guards/redirect-to-login-page.guard';
 import { DoqcmresolveService } from 'src/shared/services/doqcmresolve.service';
 import { DoqcmpageComponent } from './components/pages/doqcmpage/doqcmpage.component';
 import { InternworkspacepageComponent } from './components/pages/internworkspacepage/internworkspacepage.component';
@@ -9,12 +10,14 @@ import { DoqcmComponent } from './components/smart/doqcm/doqcm.component';
 const routes: Routes = [
   {
     path: '',
-    component: InternworkspacepageComponent
+    component: InternworkspacepageComponent,
+    canActivate: [RedirectToLoginPageGuard]
   },
 
   {
     path: 'qcm',
     component: DoqcmpageComponent,
+    canActivate: [RedirectToLoginPageGuard],
     
     children: [
       {
@@ -22,7 +25,8 @@ const routes: Routes = [
         path: 'play/:id',
         component: DoqcmComponent,
         resolve: {qcmList : DoqcmresolveService},
-        canDeactivate: [QuitQcmBeforeFinishGuard]
+        canDeactivate: [QuitQcmBeforeFinishGuard],
+        canActivate: [RedirectToLoginPageGuard]
       }
     ]
   }
