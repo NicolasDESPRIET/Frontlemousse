c97bc9b172540ed9f77171dfd948cb16217c9b4e
FINZY <william.finzy@capgemini.com>
2022-02-19 16:12:56 +0100

    Add guards for redirect when connected or when not connected

---
+++ pamplefront/src/shared/guards/redirect-home-to-workspace-when-connected.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SessionManagementService } from '../services/session-management.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectHomeToWorkspaceWhenConnectedGuard implements CanActivate {
+
+  constructor(private sessionWorker: SessionManagementService, private router: Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+    let sessionInfo: any = this.sessionWorker._getSessionInfo();
+    if(sessionInfo.isConnected && sessionInfo.userData.type=="intern"){      
+      this.router.navigate(["/intern"]);
+    }
+    if(sessionInfo.isConnected && sessionInfo.userData.type=="admin"){      
+      this.router.navigate(["/admin"]);
+    }
+    return sessionInfo;
+  }
+  
+}
